15e630ca7bb6ab3a0486426b0464a998b033eec2
ChillOutKet <60698090+ChillOutKet@users.noreply.github.com>
2020-02-08 12:37:07 -0300

    Add files via upload
    if

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+edad=document.getElementById("edad").value;
+if (edad=13){
+    alert("es adolecente");
+}else
+if (edad=14){
+    alert("es adolecente");
+}else
+if (edad=15){
+    alert("es adolecente");
+}else
+if (edad=16){
+    alert("es adolecente");
+}else
+if (edad=17){
+    alert("es adolecente");
+}else alert("no es adolecente");
 
 }//FIN DE LA FUNCIÓN
