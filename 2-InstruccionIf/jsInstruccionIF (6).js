e9cdffb4384a6b7a4808cbd5a5e9f65a678169fa
Juan Cruz Reynoso <60698090+ChillOutKet@users.noreply.github.com>
2020-02-12 12:27:38 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
 
+if (edad<13) {
+    alert("es un niño")
+}
+else if (edad<=17){
+    alert("es adolecente");
 
-
+} 
+else {
+    alert("es mayor de edad")
+}
 
 }//FIN DE LA FUNCIÓN
